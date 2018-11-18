f1b3549b85f380f631d023aebb0c7bf1ec3c65e0
Lucas Souza <lucasosouza@gmail.com>
2018-11-18 21:24:17 -0200

    added importance sampling for monte carlo (incremental implementation from Sutton book 5.6)

--- examples/test_mcimportancesampling_openai.py
+++ examples/test_mcimportancesampling_openai.py
@@ -9,22 +9,23 @@ params = {
     "ENV_NAME": "FrozenLake-v0",
     "METHOD": "DiscreteQLearning",
     "REPORTING_INTERVAL": 100,
-    "LOG_LEVEL": 1,
+    "LOG_LEVEL": 2,
     "NUMBER_EPISODES_MEAN": 10,
     "MEAN_REWARD_BOUND": .8,
     "NUM_TRIALS": 30,
     "MAX_EPISODES": 1000,
     "EPSILON_DECAY_LAST_FRAME": 4000,
     "EPSILON_START": 1.0,
-    "EPSILON_FINAL": 0,
+    "EPSILON_FINAL": 0.001, # can't be zero, since behavior policy needs to "cover" target policy
     "LEARNING_RATE": 0.3,
-    "GAMMA": 0.99
+    "GAMMA": 0.99,
+    "IMPORTANCE_SAMPLING": True
 }
 
 params["ENV_NAME"] = "FrozenLakeNotSlippery-v0"
 
 results = []
-methods = ["QLearning", "Sarsa", "FirstVisitMonteCarlo", "EveryVisitMonteCarlo", "NStepsQLearning", "NStepsSarsa"]
+methods = ["FirstVisitMonteCarlo", "EveryVisitMonteCarlo"]
 for method in methods:
     params["METHOD"] = method
     exp = UntilWinExperiment(params)
@@ -36,10 +37,10 @@ for method, result in zip(methods, results):
 
 # Results, using all same parameters:
 # For "FrozenLakeNotSlippery-v0"
-# 
-# Method QLearning took an average of 321.20 episodes
-# Method Sarsa took an average of 500.53 episodes
+
+# Method FirstVisitMonteCarlo took an average of 425.40 episodes
+# Method EveryVisitMonteCarlo took an average of 385.47 episodes
+
+# compared to non importance sampling
 # Method FirstVisitMonteCarlo took an average of 341.27 episodes
 # Method EveryVisitMonteCarlo took an average of 304.73 episodes
-# Method NStepsQLearning took an average of 287.47 episodes
-# Method NStepsSarsa took an average of 300.37 episodes
