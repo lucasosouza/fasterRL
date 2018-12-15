43d686a1c947dd6a1854d884d6a1ec69e2927314
Lucas Souza <lucasosouza@gmail.com>
2018-12-14 22:25:27 -0200

    fixed README example code

--- examples/compare_discrete_algorithms.py
+++ examples/compare_discrete_algorithms.py
@@ -27,7 +27,7 @@ results = []
 methods = ["QLearning", "Sarsa", "FirstVisitMonteCarlo", "EveryVisitMonteCarlo", "NStepsQLearning", "NStepsSarsa"]
 for method in methods:
     params["METHOD"] = method
-    o
+    exp = UntilWinExperiment(params)
     result = exp.run()
     results.append(result)
 
---
+++ examples/test.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append("../../")
+from fasterRL.common.experiment import UntilWinExperiment
+
+params = {
+    "LOG_LEVEL": 2,
+    "PLATFORM": "openai",
+    "ENV_NAME": "FrozenLake-v0",
+    "METHOD": "QLearning",
+    "NUMBER_EPISODES_MEAN": 10,
+    "MEAN_REWARD_BOUND": .8,    
+    "REPORTING_INTERVAL": 100,
+    "NUM_TRIALS": 3,
+    "MAX_EPISODES": 1000,
+    "LEARNING_RATE": 0.3,
+    "GAMMA": 0.99
+}
+
+exp = UntilWinExperiment(params)
+exp.run()
